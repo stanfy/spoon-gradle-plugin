ea862550584ecfd9c7667231831d0d6434488706
Roman Mazur <rmazur@stanfy.com.ua>
2013-10-24 08:57:28 +0300

    add devices configuration

--- src/main/groovy/com/stanfy/spoon/gradle/SpoonExtension.groovy
+++ src/main/groovy/com/stanfy/spoon/gradle/SpoonExtension.groovy
@@ -16,4 +16,8 @@ class SpoonExtension {
 
   /** Test method to be run. Used when `className` is provided. */
   String methodName
+
+  /** Devices to run the tests on (specified with serial numbers). */
+  Set<String> devices
+
 }
